5804507dbfa714b89a7326e77e3157976986fff7
deshmukhonkar <159801199+onkar384@users.noreply.github.com>
2025-11-10 23:44:06 +0530

    Add files via upload

---
+++ HuffmanEncode.cpp
@@ -0,0 +1,21 @@
+/*
+Q13. Write a program to count the occurrence of each character in a string using a HashMap. Then:
+Print all characters with occurrence > 1.
+Delete vowels from the map.
+Display remaining characters in ascending order of ASCII.
+Explanation:
+Uses counting via map.
+Shows removal of specific keys (map.remove('a'), etc.).
+Sort by using TreeMap or by converting entries into a list and sorting.
+*/
+
+import java.util.*;
+public class Map3
+{
+ public static void main (String x[])
+ {
+  Scanner xyz = new Scanner(System.in);
+  
+  
+ }
+}
